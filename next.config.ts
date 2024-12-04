2fea775c11748dd719cb9f2b8d47cc01ace23b41
王 雪 <124015253+x-wang12345@users.noreply.github.com>
2024-12-04 11:13:21 +0900

    Create nextjs.yml

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "export",
+  basePath: "/docs",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
